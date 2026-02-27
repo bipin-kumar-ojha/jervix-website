82bd8bdc26c0064fb0d1be2fcae72cc5f670b5a9
Bipin Kumar <bipinkumarojha123@gmail.com>
2026-02-27 22:03:49 +0530

    bugs fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
 import Header from './components/layout/Header'
-import UnderDevelopmentPopup from './components/common/UnderDevelopmentPopup';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Footer from './components/layout/Footer/Footer';
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,6 @@ import FourBlock from "../../components/FourBlockSection/FourBlock";
 import Featurex from "../../components/Features/Featurex";
 import Services from "../../components/Services/services";
 import InfoGrid from "../../components/InfoGrid/InfoGrid";
-import Price from "../../components/Price/Price";
 import Blog from "../../components/blog/blog";
 import Subscription from "../../components/subscription/subscription";
 export default function Home() {
